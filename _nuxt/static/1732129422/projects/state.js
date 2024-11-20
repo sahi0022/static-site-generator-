8f14cf266c440ff9cdb32b2aa9ab546cb4dc2b1c
sahi0022 <133886972+sahi0022@users.noreply.github.com>
2024-11-21 00:08:47 +0500

    Fixed

---
+++ _nuxt/static/1732129422/projects/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fstatic-site-generator-\u002F_nuxt\u002Fstatic\u002F1732129422",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects",config:{_app:{basePath:"\u002Fstatic-site-generator-\u002F",assetsPath:"\u002Fstatic-site-generator-\u002F_nuxt\u002F",cdnURL:a}}}}(null));
